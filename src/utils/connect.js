3c56205e90e4cc0c29bb6b8c57e4f9b7febf80b2
Soufianetyk1337 <sz.fanel@gmail.com>
2021-12-14 03:29:03 +0100

    Refactoring mongoose connection file

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
-import { env } from "process";
+import process, { env } from "process";
 import { logger } from "./logger.js";
 
-const connect = async () => {
+const connect = () => {
   const dbURI = env.DB_URI;
-
-  await mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.connection
+    .on('error', logger.error)
+    .on('disconnected', connect)
+  return mongoose.connect(dbURI, {
+    keepAlive: 1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
     .then(() => logger.success("Successfully connected to the database"))
     .catch((error) => {
-      console.log(error);
+      logger.error(error);
       process.exit(1);
     })
 }
