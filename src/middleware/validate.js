8ce064c70d86ccf778e8a2a7e5bcefe24a6b4c4f
Soufianetyk1337 <sz.fanel@gmail.com>
2021-12-14 03:30:34 +0100

    Helper function to validate mongoose schemas

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,11 +1,9 @@
 export const validate = (schema) => async (req, res, next) => {
   try {
-      await schema.validate({
-     body: req.body,
-    //   query: req.query,
-    //   params: req.params,
+    await schema.validate({
+      body: req.body,
     })
-   next();
+    next();
   } catch (error) {
     res.status(400).send(error.message);
   }
