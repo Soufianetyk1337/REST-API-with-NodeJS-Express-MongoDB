36a86a19306c632238af252209b2151cff42ad8f
Soufianetyk1337 <sz.fanel@gmail.com>
2021-12-15 23:41:41 +0100

    Helper functions fot the user controller

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,28 +1,28 @@
-import mongoose from 'mongoose'
-import {
-    UserModel
-} from '../model/user.model.js'
-import {
-    logger
-} from './../utils/logger.js'
-import omit from 'lodash/omit.js'
+import { UserModel } from "../model/user.model.js";
+import omit from "lodash/omit.js";
 export const createUser = async (req, res) => {
     try {
         const userData = req.body;
-        const createdUser = await UserModel.create(userData)
-        return omit(createdUser.toJSON(), ["password"])
-    } catch (error) {
-        logger.error(error.message)
-        res.send(error.message)
+        const createdUser = await UserModel.create(userData);
+        if (!createdUser) {
+            return res
+                .status(400)
+                .json({ success: false, message: "Email already Exists!" });
+        }
+        return omit(createdUser.toJSON(), ["password", "confirmPassword"]);
+    } catch (err) {
+        return err;
     }
-}
+};
 
 export const validateUserPassword = async ({ email, password }) => {
-    const user = await UserModel.findOne({ email })
+    const user = await UserModel.findOne({ email });
     if (!user) {
         return false;
     }
-    const passwordMatches = await user.comparePassword(password)
-    if (!passwordMatches) { return false; }
-    return omit(user.toJSON(), ["password"])
-}
+    const passwordMatches = await user.comparePassword(password);
+    if (!passwordMatches) {
+        return false;
+    }
+    return omit(user.toJSON(), ["password"]);
+};
