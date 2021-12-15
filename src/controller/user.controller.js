e7c94c103ac31b23f756c536e54cbb0131e21a0a
Soufianetyk1337 <sz.fanel@gmail.com>
2021-12-15 23:47:03 +0100

    User controller functions

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,130 +1,197 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-import { logger } from "../utils/logger.js";
+
 import { createUser } from "../service/user.service.js";
-import omit from "lodash/omit.js"
-import { UserModel } from '../model/user.model.js'
-import { createHmac, randomBytes } from 'crypto'
-import nodemailer from "nodemailer"
-import dayjs from "dayjs"
-import { env } from 'process'
-import mongoose from 'mongoose';
-import crypto from 'crypto'
-import { PasswordResetModel } from '../model/passwordReset.model.js'
-import { getEmailProvider } from '../helpers/getEmailProvider.js'
-import { html, compress } from '../helpers/emailTemplate.js'
-import { limitSlowBruteForceByIp, limitFastBruteForceByIp } from '../middleware/rateLimiter.js'
-import { add } from "lodash-es";
+import { UserModel } from "../model/user.model.js";
+import { verify } from "crypto";
+import nodemailer from "nodemailer";
+import { env } from "process";
+import { PasswordResetModel } from "../model/passwordReset.model.js";
+import { tokenGenerator } from "../helpers/tokenGenerator.js";
+import { passwordForgot } from "../template/passwordForgot.js";
+import { emailVerification } from "../template/emailVerification.js";
+import { verifyJwt } from "../utils/jwt_utils.js";
+
 export const createUserHandler = async (req, res) => {
   try {
     const user = await createUser(req, res);
-    return res.status(201).send(user)
+    if (user.code === 11000) {
+      return res.status(409).json({ success: false, message: "Email address already in use" });
+
+    }
+    const token = tokenGenerator(user.email);
+    const link = `${env.BASE_URL}/users/email/verify?id=${user._id}&verificationToken=${token}`;
+    let mailOptions = {
+      from: '"John Doe" <john-doe@gmail.com>',
+      to: user.email,
+      subject: "email verification",
+      html: emailVerification(
+        link,
+        user.name,
+        env.PASSWORD_RESET_EXPIRY_IN_MINUTES || 15,
+        verify
+      ),
+    };
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      service: "gmail",
+      auth: {
+        user: env.NODEMAILER_USER,
+        pass: env.NODEMAILER_PASS,
+      },
+    });
+    const mailSent = await transporter.sendMail(mailOptions);
+    if (mailSent) {
+      return res.status(201).json({
+        success: "true",
+        message:
+          "You have successfully created your account. Check your email to verify it.",
+        created: { user },
+      });
+    } else {
+      return res.status(409).json({ mailSent });
+    }
   } catch (error) {
-    logger.error(error);
-    return res.status(409).send(`error from controller :${error}`);
+    return res.status(409).json({ success: false, message: "Invalid Email" });
   }
 };
-
 
 export const createUserPasswordReset = async (req, res) => {
   const { id, token } = req.query;
   const { newPassword } = req.body;
-  const passwordResetAttempt = await PasswordResetModel.findOne({ userId: id })
-
-  // Check if the link is still valid 
-  const isLinkStillValid = dayjs(passwordResetAttempt.createdAt).add(+env.PASSWORD_RESET_EXPIRY_IN_MINUTES, 'minute').valueOf()
-  console.log(isLinkStillValid)
-  console.log(Date.now())
-  const expiresAt = dayjs().add(+env.PASSWORD_RESET_EXPIRY_IN_MINUTES, 'minute').unix()
-  if (isLinkStillValid < Date.now()) {
-    return res.status(400).send("Link expired.")
+  if (!id || !token) {
+    return res.status(401).json({
+      success: "false",
+      message: "Please check your email for reset password link",
+    });
   }
-  const user = await UserModel.findById(id)
-  user.password = newPassword
-  user.updateOne((error, record) => {
-    if (error) {
-      console.log(error)
-    };
-  })
-
-  return res.status(200).json({ success: true, message: "You have succesfully reset your password" })
-}
+  const { valid, expired } = verifyJwt(token);
+  if (valid && !expired) {
+    // eslint-disable-next-line no-unused-vars
+    const passwordResetAttempt = await PasswordResetModel.findOne({
+      userId: id,
+    });
+    const user = await UserModel.findById(id);
+    if (!user) {
+      return res.status(400).send("Something went wrong try again later.");
+    }
+    user.password = await user.hashPassword(newPassword);
+    await user.save();
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "You have succesfully reset your password",
+      });
+  } else {
+    return res.status(400).json({
+      success: false,
+      message: "Your password reset link expired",
+    });
+  }
+};
 // User should confirm his password if he changes some critical data
 export const createUserPasswordConfirm = async (req, res) => {
   const { password } = req.body;
-  const { sid } = req.session
-  const user = await UserModel.findById(id)
+  const { sid } = req.session;
+  const user = await UserModel.findById(sid);
   if (!user) {
-    return res.status(400).json({ message: "User not found" })
+    return res.status(400).json({ message: "User not found" });
   }
-  const passwordMatches = await user.comparePassword(password)
+  const passwordMatches = await user.comparePassword(password);
   if (!passwordMatches) {
-    return res.status(401).json({ message: "Password is incorrect" })
+    return res.status(401).json({ message: "Password is incorrect" });
   }
   req.session.passwordConfirmedAt = Date.now();
-  return res.status(200).json({ message: "Password confirmed successfully" })
-}
-
+  return res.status(200).json({ message: "Password confirmed successfully" });
+};
 
 export const createUserEmailVerification = async (req, res) => {
-  console.log(req.ip)
-  const ipAddress = req.connection.remoteAddress;
-  const [responseFastBruteForceByIp, responseSlowBruteForceByIp] = await Promise.all([
-    limitFastBruteForceByIp.get(ipAddress),
-    limitSlowBruteForceByIp.get(ipAddress),
-  ])
-  console.log(responseSlowBruteForceByIp)
-  const { email } = req.body;
-  const emailProvider = getEmailProvider(email).toLowerCase()
-  console.log(emailProvider)
-  res.json({ emailProvider })
+  const { id, verificationToken } = req.query;
+  if (!id || !verificationToken) {
+    return res.status(401).json({
+      success: "false",
+      message: "Please check your email for verification link",
+    });
+  }
 
-}
+  const { valid, expired } = verifyJwt(verificationToken);
+  if (valid && !expired) {
+    const user = await UserModel.findByIdAndUpdate(id, { confirmed: true });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "You have succesfully verified your account",
+    });
+  } else {
+    return res.status(400).json({
+      success: false,
+      message: "Your verification link expired",
+    });
+  }
+};
 
 export const createUserPasswordForgot = async (req, res) => {
-  const { email } = req.body
-  const user = await UserModel.findOne({ email })
+  const { email } = req.body;
+  const user = await UserModel.findOne({ email });
   if (!user) {
-    return res.status(400).send("Invalid Email.")
+    return res.status(400).send("Invalid Email.");
   }
   const userId = user._id;
-  const previousPasswordResetAttempt = await PasswordResetModel.findOne({ userId })
+  const previousPasswordResetAttempt = await PasswordResetModel.findOne({
+    userId,
+  });
   if (previousPasswordResetAttempt) {
-    const previousPasswordResetDoc = await PasswordResetModel.deleteOne({ userId })
-    // if (previousPasswordResetDoc) {
-    //   console.log(previousPasswordResetDoc)
-    // }
+    // eslint-disable-next-line no-unused-vars
+    const previousPasswordResetDoc = await PasswordResetModel.deleteOne({
+      userId,
+    });
   }
-  const randomByte = randomBytes(+env.PASSWORD_RESET_TOKEN_LENGTH).toString("hex")
-  const token = createHmac("sha256", env.PASSWORD_RESET_SECRET).update(randomByte).digest("hex")
-
-  const passwordResetAttempt = await PasswordResetModel.create({ userId, email, token })
+  const token = tokenGenerator();
+  const passwordResetAttempt = await PasswordResetModel.create({
+    userId,
+    email,
+    token,
+  });
   if (!passwordResetAttempt) {
     return res.status(500).send("Something went wrong. Try again later");
   }
-  const link = `${env.BASE_URL}/users/reset-password?id=${user._id}&token=${token}`
+  const link = `${env.BASE_URL}/users/password/reset?id=${user._id}&token=${token}`;
   let mailOptions = {
-    from: 'test@gmail.com',
-    // to: "sz.fanel@gmail.com" || user.email,
+    from: '"John Doe" <john-doe@gmail.com>',
     to: user.email,
-    subject: 'Reset your account password',
-    html: compress(html(link, user.name, env.PASSWORD_RESET_EXPIRY_IN_MINUTES || 15))
-  }
+    subject: "Reset your account password",
+    html: passwordForgot(
+      link,
+      user.name,
+      env.PASSWORD_RESET_EXPIRY_IN_MINUTES || 15
+    ),
+  };
 
   let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
     service: "gmail",
     auth: {
       user: env.NODEMAILER_USER,
-      pass: env.NODEMAILER_PASS
-    }
-  })
-  const mailSent = await transporter.sendMail(mailOptions)
+      pass: env.NODEMAILER_PASS,
+    },
+  });
+  const mailSent = await transporter.sendMail(mailOptions);
   if (mailSent) {
-    return res.status(200).json({ success: true, userId, token })
+    return res.status(200).json({ success: true, message: "Check your email for reset link" });
   } else {
-    return res.status(400).json({ message: "Unable to send email." })
+    return res.status(400).json({ success: false, message: "Unable to send email." });
   }
-}
+};
+
+export const createUserSendEmailVerification = (req, res) => { };
+/**
+ * @description
+ * @api
+ * @access
+ * @type
+ */
