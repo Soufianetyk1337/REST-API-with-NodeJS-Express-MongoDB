b95831bc0eaa64fcd5b7078c540c401094dff283
Soufianetyk1337 <sz.fanel@gmail.com>
2021-12-15 23:46:13 +0100

    Route for the REST API

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,74 @@
-import { createUserHandler } from './controller/user.controller.js'
-import { createUserSessionHandler, deleteUserSessionHandler, getUserSessionHandler } from './controller/session.controller.js'
-import { createUserValidationSchema } from './schema/user.schema.js'
-import { createSessionValidationSchema } from './schema/session.schema.js'
-import { validate } from './middleware/validate.js'
-import { userRequired } from './middleware/userRequired.js'
+import {
+  createUserHandler,
+  createUserPasswordConfirm,
+  createUserPasswordReset,
+  createUserEmailVerification,
+  createUserPasswordForgot,
+} from "./controller/user.controller.js";
+import {
+  createUserSessionHandler,
+  deleteUserSessionHandler,
+} from "./controller/session.controller.js";
+import {
+  createEmailValidationSchema,
+  createPasswordValidationSchema,
+  createUserValidationSchema,
+} from "./schema/user.schema.js";
+import { createSessionValidationSchema } from "./schema/session.schema.js";
+import { validate } from "./middleware/validate.js";
+import { userIsGuest } from "./middleware/userIsGuest.js";
+import { userIsLoggedIn } from "./middleware/userIsLoggedIn.js";
 
+import { passwordBruteforceRateLimiterMiddleware } from "./middleware/rateLimiter.js";
+import { userIsVerified } from "./middleware/userIsVerified.js";
+import { checkEmailExistence } from "./middleware/checkEmailExistence.js";
+// req.headers['x-forwarded-for'] || req.connection.remoteAddress
 export const routes = (app) => {
-  app.get("/ping", (req, res) => {
-    res.sendStatus(200);
+  app.get("/api/v1/ping", (req, res) => {
+    return res.sendStatus(200);
   });
-  app.post("/api/users", validate(createUserValidationSchema), createUserHandler)
-  app.post("/api/sessions", validate(createSessionValidationSchema), createUserSessionHandler)
-  app.get("/api/sessions", userRequired, getUserSessionHandler)
-  app.delete("/api/sessions", userRequired, deleteUserSessionHandler)
-
+  app.post(
+    "/api/v1/users/register",
+    userIsGuest,
+    checkEmailExistence,
+    validate(createUserValidationSchema),
+    createUserHandler
+  );
+  app.post(
+    "/api/v1/users/login",
+    userIsGuest,
+    validate(createSessionValidationSchema),
+    passwordBruteforceRateLimiterMiddleware,
+    createUserSessionHandler
+  );
+  app.delete(
+    "/api/v1/users/logout",
+    userIsLoggedIn,
+    userIsVerified,
+    deleteUserSessionHandler
+  );
+  app.patch(
+    "/api/v1/users/password/reset",
+    userIsGuest,
+    validate(createPasswordValidationSchema),
+    createUserPasswordReset
+  );
+  app.post(
+    "/api/v1/users/password/confirm",
+    userIsLoggedIn,
+    userIsVerified,
+    validate(createPasswordValidationSchema),
+    createUserPasswordConfirm
+  );
+  app.post(
+    "/api/v1/users/password/forgot",
+    userIsGuest,
+    validate(createEmailValidationSchema),
+    createUserPasswordForgot
+  );
+  app.get(
+    "/api/v1/users/email/verify",
+    userIsGuest,
+    createUserEmailVerification
+  );
 };
