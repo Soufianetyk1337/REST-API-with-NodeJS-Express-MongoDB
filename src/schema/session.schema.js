8e2fcbaefd8d89f296d59e858d51f246e212da56
Soufianetyk1337 <sz.fanel@gmail.com>
2021-12-15 23:43:33 +0100

    Adding more constraints to validate user login

--- src/schema/session.schema.js
+++ src/schema/session.schema.js
@@ -2,7 +2,13 @@ import * as yup from 'yup';
 import { object } from 'yup';
 export const createSessionValidationSchema = object().shape({
   body: yup.object({
-    email: yup.string().required("Email field is required").email("Not a valid email"),
-    password: yup.string().required("Password field is required").min(6, "Password must be greater than 6 characters"),
+    email: yup.string().required("Email field is required")
+      .email("Not a valid email").min(8).max(254)
+      .lowercase().trim(),
+    password: yup.string()
+      .required("Password is required")
+      .min(6, "Password must be greater than 6 characters")
+      .max(72, 'utf8')
+      .matches(/^(?=.*?[\p{Lu}])(?=.*?[\p{Ll}])(?=.*?\d).*$/u, { message: "password must contain one uppercase letter, one lowercase letter, and one digit." }),
   })
 });
