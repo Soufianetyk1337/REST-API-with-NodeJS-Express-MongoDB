2fdd37baee7ccdbe7321eb03770e88bd601ff3ea
Soufianetyk1337 <sz.fanel@gmail.com>
2023-08-29 12:33:03 +0100

    Adjusting redis config for render

--- src/config/session.js
+++ src/config/session.js
@@ -6,32 +6,30 @@ import { logger } from "../utils/logger.js";
 const HALF_HOUR = 1000 * 60 * 30;
 const RedisStore = connectRedis(session);
 const redisClient = new Redis({
-    host: env.REDIS_HOST,
-    port: env.REDIS_PORT,
-    connectTimeout: 10000,
+  connectTimeout: 10000,
   url: env.REDIS_URL,
-    family: 4,
+  family: 4,
 });
 
 redisClient.on("connect", function (err) {
-    logger.success("Connected to redis successfully");
+  logger.success("Connected to redis successfully");
 });
 
 redisClient.on("error", function (err) {
-    logger.error(err.stack);
+  logger.error("Redis Client Error", err);
 });
 
 export const sessionConfig = {
-    name: "sid",
-    secret: env.SESSION_SECRET,
-    maxDuration: 60 * 60 * 6, // duration in seconds (this would be 6 hours)
-    cookie: {
-        maxAge: env.SESSION_TIME_TO_LIVE || HALF_HOUR,
-        sameSite: true,
-        secure: env.NODE_ENV === "production",
-    },
-    rolling: true,
-    resave: false,
-    saveUninitialized: false,
-    store: new RedisStore({ client: redisClient, ttl: 86400 }),
+  name: "sid",
+  secret: env.SESSION_SECRET,
+  maxDuration: 60 * 60 * 6, // duration in seconds (this would be 6 hours)
+  cookie: {
+    maxAge: env.SESSION_TIME_TO_LIVE || HALF_HOUR,
+    sameSite: true,
+    secure: env.NODE_ENV === "production",
+  },
+  rolling: true,
+  resave: false,
+  saveUninitialized: false,
+  store: new RedisStore({ client: redisClient, ttl: 86400 }),
 };
