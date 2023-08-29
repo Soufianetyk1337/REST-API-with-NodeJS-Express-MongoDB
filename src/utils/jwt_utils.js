1adfa1b0a0e43c962159fef1ec2c44f2143e4292
Soufianetyk1337 <sz.fanel@gmail.com>
2023-08-29 09:28:27 +0100

    Reading keys from .env instead of files

--- src/utils/jwt_utils.js
+++ src/utils/jwt_utils.js
@@ -1,38 +1,33 @@
-import { readKeyFromFile } from '../helpers/readKeyFromFile.js'
-const privateKey = readKeyFromFile("private_key.pem");
-const publicKey = readKeyFromFile("public_key.pem");
+/* eslint-disable no-undef */
+const privateKey = process.env.PRIVATE_KEY;
+const publicKey = process.env.PUBLIC_KEY;
 import jwt from "jsonwebtoken";
-import { logger } from '../utils/logger.js'
+import { logger } from "../utils/logger.js";
 export const signJwt = (payload, options = {}) => {
-    try {
-        const formattedPrivateKey = privateKey;
-        const jwtToken = jwt.sign(
-            { payload },
-            formattedPrivateKey,
-            {
-                ...(options && options),
-                algorithm: "RS256"
-            })
-        return jwtToken;
-
-    } catch (error) {
-        logger.error("Error", error)
-    }
-
-}
+  try {
+    const formattedPrivateKey = privateKey;
+    const jwtToken = jwt.sign({ payload }, formattedPrivateKey, {
+      ...(options && options),
+      algorithm: "RS256",
+    });
+    return jwtToken;
+  } catch (error) {
+    logger.error("Error", error);
+  }
+};
 export const verifyJwt = (token) => {
-    try {
-        const decoded = jwt.verify(token, publicKey);
-        return {
-            valid: true,
-            expired: false,
-            decoded: decoded
-        }
-    } catch (error) {
-        return {
-            valid: false,
-            expired: error.message === 'jwt expired',
-            decoded: null
-        }
-    }
-}
+  try {
+    const decoded = jwt.verify(token, publicKey);
+    return {
+      valid: true,
+      expired: false,
+      decoded: decoded,
+    };
+  } catch (error) {
+    return {
+      valid: false,
+      expired: error.message === "jwt expired",
+      decoded: null,
+    };
+  }
+};
